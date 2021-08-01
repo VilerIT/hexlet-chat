415e0f17f6d341d885313462bde8ebbf8a5b89d3
VilerIT <vilerinftech@gmail.com>
2021-08-01 19:11:20 +0400

    set up environment

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const {
+  src,
+  dest,
+  watch,
+  series,
+} = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const pug = require('gulp-pug');
+const svgSprite = require('gulp-svg-sprite');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+const scripts = () => {
+  return src([
+    './node_modules/jquery/dist/jquery.min.js',
+  ])
+    .pipe(dest('./build/js/'));
+};
+
+const sass2css = () => {
+  return src('app/scss/app.scss')
+    .pipe(sass())
+    .pipe(concat('app.css'))
+    .pipe(dest('./build/styles/'))
+    .pipe(browserSync.stream());
+};
+
+const pug2html = () => {
+  return src([
+    'app/pages/index.pug',
+  ])
+    .pipe(pug())
+    .pipe(dest('./build/'))
+    .pipe(browserSync.stream());
+};
+
+const svg2sprite = () => {
+  const config = {
+    mode: {
+      stack: {
+        sprite: '../sprite.svg',
+      },
+    },
+  };
+
+  return src([
+    'app/images/icons/*.svg',
+  ])
+    .pipe(svgSprite(config))
+    .pipe(dest('./build/images/icons/'));
+};
+
+const initBrowserSync = () => {
+  browserSync.init({
+    server: {
+      baseDir: './build/',
+    },
+  });
+
+  watch('./app/scss/**/*.scss', sass2css);
+  watch('./app/pages/**/*.pug', pug2html);
+};
+
+exports.default = series(
+  pug2html,
+  sass2css,
+  scripts,
+  svg2sprite,
+  initBrowserSync,
+);
